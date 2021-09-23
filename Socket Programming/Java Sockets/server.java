c5e8cbfd45758e03c3fb7fc741efcd6a3b0ddb7c
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2021-09-23 05:48:28 +0530

    Update server.java

--- Socket Programming/Java Sockets/server.java
+++ Socket Programming/Java Sockets/server.java
@@ -1,3 +1,7 @@
+/*
+This is a simple java http server
+after running this file open localhost:9999 in browser.
+ */
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,7 +24,7 @@ public class server {
                 // a simple response
                 String httpResponse = "HTTP/1.1\r\n\r\n"+"Hey there ";
                 client.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-                break;
+
 
             }catch (Exception e){
                 System.out.print(e);

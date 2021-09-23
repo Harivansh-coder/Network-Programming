8aa8570615cbb41cd88750847b8c9bf16776919d
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2021-09-23 05:43:46 +0530

    initial commit

---
+++ Socket Programming/Java Sockets/server.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+
+        // Socket for server created
+        final ServerSocket serverSocket = new ServerSocket(9999);
+
+        System.out.println("Server listening at port 9999");
+
+        Socket client = serverSocket.accept();
+
+        while(client != null){
+            try {
+                // client
+                System.out.println("Connected to: "+client.getRemoteSocketAddress());
+
+                // a simple response
+                String httpResponse = "HTTP/1.1\r\n\r\n"+"Hey there ";
+                client.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                break;
+
+            }catch (Exception e){
+                System.out.print(e);
+            }
+        }
+    }
+}
